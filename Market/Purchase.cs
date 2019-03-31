3812e6997165a601c2b45698eee1acd58741025e
Dmitry <danilindmitr99@icloud.com>
2019-04-01 01:58:46 +0300

    Work with market data
    Add BuyItem, SellItem

---
+++ Market/Purchase.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using BitSkinsApi.Extensions;
+
+namespace BitSkinsApi.Market
+{
+    /// <summary>
+    /// Work with the purchase.
+    /// </summary>
+    public static class Purchase
+    {
+        /// <summary>
+        /// Allows you to buy the item currently on sale on BitSkins. Item must not be currently be on sale to you.
+        /// </summary>
+        /// <param name="app">For the inventory's game.</param>
+        /// <param name="itemIds">List of item IDs.</param>
+        /// <param name="itemPrices">Prices at which you want to make the purchase. Important to specify this in case the prices change by the time you make this call.</param>
+        /// <param name="autoTrade">Initiate trade offer for purchased items' delivery.</param>
+        /// <param name="allowTradeDelayedPurchases">Use 'true' if you want to purchase items that are trade-delayed.</param>
+        /// <returns>List of purchased items.</returns>
+        public static List<PurchasedItem> BuyItem(AppId.AppName app, List<string> itemIds, List<double> itemPrices, 
+            bool autoTrade, bool allowTradeDelayedPurchases)
+        {
+            string delimiter = ",";
+
+            StringBuilder itemIdsStr = new StringBuilder();
+            for (int i = 0; i < itemIds.Count; i++)
+            {
+                itemIdsStr.Append(itemIds[i]);
+                itemIdsStr.Append((i < itemIds.Count - 1) ? delimiter : "");
+            }
+
+            StringBuilder itemPricesStr = new StringBuilder();
+            for (int i = 0; i < itemPrices.Count; i++)
+            {
+                itemPricesStr.Append(itemPrices[i].ToString().Replace(',', '.'));
+                itemPricesStr.Append((i < itemPrices.Count - 1) ? delimiter : "");
+            }
+
+            StringBuilder url = new StringBuilder($"https://bitskins.com/api/v1/buy_item/");
+            url.Append($"?api_key={Account.AccountData.GetApiKey()}");
+            url.Append($"&app_id={(int)app}");
+            url.Append($"&item_ids={itemIdsStr.ToString()}");
+            url.Append($"&prices={itemPricesStr.ToString()}");
+            url.Append($"&auto_trade={autoTrade}");
+            url.Append($"&allow_trade_delayed_purchases={allowTradeDelayedPurchases}");
+            url.Append($"&code={Account.Secret.GetTwoFactorCode()}");
+            
+            string result = Server.ServerRequest.RequestServer(url.ToString());
+            List<PurchasedItem> purchasedItems = ReadPurchasedItems(result);
+            return purchasedItems;
+        }
+
+        static List<PurchasedItem> ReadPurchasedItems(string result)
+        {
+            dynamic responseServer = JsonConvert.DeserializeObject(result);
+            dynamic items = responseServer.data.items;
+
+            if (items == null)
+            {
+                return new List<PurchasedItem>();
+            }
+
+            List<PurchasedItem> purchasedItems = new List<PurchasedItem>();
+            foreach (dynamic item in items)
+            {
+                Console.WriteLine(item.item_id);
+                string itemId = item.item_id;
+                string marketHashName = item.market_hash_name;
+                double price = item.price;
+                DateTime withdrawableAt = DateTimeExtension.FromUnixTime((long)item.withdrawable_at);
+
+                PurchasedItem purchasedItem = new PurchasedItem(itemId, marketHashName, price, withdrawableAt);
+                purchasedItems.Add(purchasedItem);
+            }
+
+            return purchasedItems;
+        }
+    }
+
+    /// <summary>
+    /// Purchased item BitSkins.
+    /// </summary>
+    public class PurchasedItem
+    {
+        public string ItemId { get; private set; }
+        public string MarketHashName { get; private set; }
+        public double Price { get; private set; }
+        public DateTime WithdrawableAt { get; private set; }
+
+        internal PurchasedItem(string itemId, string marketHashName, double price, DateTime withdrawableAt)
+        {
+            ItemId = itemId;
+            MarketHashName = marketHashName;
+            Price = price;
+            WithdrawableAt = withdrawableAt;
+        }
+    }
+}
---
+++ Market/Sale.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BitSkinsApi.Market
+{
+    /// <summary>
+    /// Work with sale.
+    /// </summary>
+    public static class Sale
+    {
+        /// <summary>
+        /// Allows you to list an item for sale. This item comes from your Steam inventory. If successful, bots will ask you to trade in the item you want listed for sale. 
+        /// </summary>
+        /// <param name="app">For the inventory's game.</param>
+        /// <param name="itemIds">List of item IDs from your Steam inventory.</param>
+        /// <param name="itemPrices">List of prices for each item ID you want to list for sale.</param>
+        /// <returns>Info about sale.</returns>
+        public static SaleInformation SellItem(AppId.AppName app, List<string> itemIds, List<double> itemPrices)
+        {
+            string delimiter = ",";
+
+            StringBuilder itemIdsStr = new StringBuilder();
+            for (int i = 0; i < itemIds.Count; i++)
+            {
+                itemIdsStr.Append(itemIds[i]);
+                itemIdsStr.Append((i < itemIds.Count - 1) ? delimiter : "");
+            }
+
+            StringBuilder itemPricesStr = new StringBuilder();
+            for (int i = 0; i < itemPrices.Count; i++)
+            {
+                itemPricesStr.Append(itemPrices[i].ToString().Replace(',', '.'));
+                itemPricesStr.Append((i < itemPrices.Count - 1) ? delimiter : "");
+            }
+
+            StringBuilder url = new StringBuilder($"https://bitskins.com/api/v1/list_item_for_sale/");
+            url.Append($"?api_key={Account.AccountData.GetApiKey()}");
+            url.Append($"&app_id={(int)app}");
+            url.Append($"&item_ids={itemIdsStr.ToString()}");
+            url.Append($"&prices={itemPricesStr.ToString()}");
+            url.Append($"&code={Account.Secret.GetTwoFactorCode()}");
+            
+            string result = Server.ServerRequest.RequestServer(url.ToString());
+            SaleInformation saleInformation = ReadSalesInformation(result);
+            return saleInformation;
+        }
+
+        static SaleInformation ReadSalesInformation(string result)
+        {
+            dynamic responseServer = JsonConvert.DeserializeObject(result);
+            dynamic items = responseServer.data.items;
+            dynamic tradeTokensD = responseServer.data.trade_tokens;
+            dynamic botInfo = responseServer.data.bot_info;
+
+            List<SoldItem> soldItems = new List<SoldItem>();
+            if (items != null)
+            {
+                foreach (dynamic item in items)
+                {
+                    string itemId = item.item_id;
+
+                    SoldItem soldItem = new SoldItem(itemId);
+                    soldItems.Add(soldItem);
+                }
+            }
+
+            List<string> tradeTokens = new List<string>();
+            if (tradeTokensD != null)
+            {
+                foreach (dynamic token in tradeTokensD)
+                {
+                    tradeTokens.Add((string)token);
+                }
+            }
+
+            SaleBotInfo saleBotInfo = null;
+            if (botInfo != null)
+            {
+                string botUid = botInfo.uid;
+                string namePrefix = botInfo.name_prefix;
+
+                saleBotInfo = new SaleBotInfo(botUid, namePrefix);
+            }
+
+            SaleInformation saleInformation = new SaleInformation(soldItems, tradeTokens, saleBotInfo);
+            return saleInformation;
+        }
+    }
+
+    /// <summary>
+    /// Info about sale.
+    /// </summary>
+    public class SaleInformation
+    {
+        public List<SoldItem> SoldItems { get; private set; }
+        public List<string> TradeTokens { get; private set; }
+        public SaleBotInfo SaleBotInfo { get; private set; }
+
+        internal SaleInformation(List<SoldItem> soldItems, List<string> tradeTokens, SaleBotInfo saleBotInfo)
+        {
+            SoldItems = soldItems;
+            TradeTokens = tradeTokens;
+            SaleBotInfo = saleBotInfo;
+        }
+    }
+
+    /// <summary>
+    /// Info about sold item.
+    /// </summary>
+    public class SoldItem
+    {
+        public string ItemId { get; private set; }
+
+        internal SoldItem(string itemId)
+        {
+            ItemId = itemId;
+        }
+    }
+
+    /// <summary>
+    /// Information about the bot that offers the trade.
+    /// </summary>
+    public class SaleBotInfo
+    {
+        public string Uid { get; private set; }
+        public string NamePrefix { get; private set; }
+
+        internal SaleBotInfo(string uid, string namePrefix)
+        {
+            Uid = uid;
+            NamePrefix = namePrefix;
+        }
+    }
+}
