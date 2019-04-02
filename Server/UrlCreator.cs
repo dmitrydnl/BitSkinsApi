723e1903f22a2e5c45b53311b976d2b0a441af46
Dmitry <danilindmitr99@icloud.com>
2019-04-02 17:01:58 +0300

    Work with server requests
    Add UrlCreator

---
+++ Server/UrlCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace BitSkinsApi.Server
+{
+    class UrlCreator
+    {
+        private readonly StringBuilder Url;
+
+        public UrlCreator(string urlStart)
+        {
+            Url = new StringBuilder(urlStart);
+            Url.Append($"?api_key={Account.AccountData.GetApiKey()}");
+            Url.Append($"&code={Account.Secret.GetTwoFactorCode()}");
+        }
+
+        internal void AppendUrl(string url)
+        {
+            Url.Append(url);
+        }
+
+        internal string ReadUrl()
+        {
+            return Url.ToString();
+        }
+    }
+}
