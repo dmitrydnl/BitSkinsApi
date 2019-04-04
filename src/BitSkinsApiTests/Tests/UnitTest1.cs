d5db6f05cad7989fa60d368e94661439c2876287
Dmitry <danilindmitr99@icloud.com>
2019-04-05 01:16:07 +0300

    Add Tests project

---
+++ src/BitSkinsApiTests/Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitSkinsApiTests
+{
+    [TestClass]
+    public class Class
+    {
+        [TestMethod]
+        public void Method()
+        {
+
+        }
+    }
+}
