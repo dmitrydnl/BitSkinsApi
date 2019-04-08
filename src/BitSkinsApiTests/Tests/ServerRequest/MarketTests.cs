47c78b420a6105ab6a1e2fe6a6922d7f976de905
Dmitry <danilindmitr99@icloud.com>
2019-04-08 13:38:10 +0300

    Update tests
    Add RelistAndDelistItemTest, ModifySaleTest, BuyItemTest, SellItemTest

--- src/BitSkinsApiTests/Tests/ServerRequest/MarketTests.cs
+++ src/BitSkinsApiTests/Tests/ServerRequest/MarketTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BitSkinsApi.Market;
+using BitSkinsApi.Inventory;
 
 namespace BitSkinsApiTests.ServerRequest
 {
@@ -117,7 +118,7 @@ namespace BitSkinsApiTests.ServerRequest
 
             foreach (KeyValuePair<string, AppId.AppName> item in appItems)
             {
-                for(int page = 1; page <= 5; page++)
+                for (int page = 1; page <= 5; page++)
                 {
                     List<ItemRecentSale> recentSaleItems = RecentSaleInfo.GetRecentSaleInfo(item.Value, item.Key, page);
                 }
@@ -199,6 +200,104 @@ namespace BitSkinsApiTests.ServerRequest
                     page++;
                     resetPriceItems = ResetPriceItems.GetResetPriceItems(appId, page);
                 }
+            }
+        }
+
+        [TestMethod]
+        public void RelistAndDelistItemTest()
+        {
+            AppId.AppName app = AppId.AppName.CounterStrikGlobalOffensive;
+            BitSkinsInventoryItem item = null;
+            foreach (AppId.AppName appId in Enum.GetValues(typeof(AppId.AppName)))
+            {
+                AccountInventory accountInventorys = Inventories.GetAccountInventory(appId, 1);
+                if (accountInventorys.BitSkinsInventory.TotalItems > 0)
+                {
+                    item = accountInventorys.BitSkinsInventory.BitSkinsInventoryItems[0];
+                    app = appId;
+                    break;
+                }
+            }
+
+            if (item != null)
+            {
+                string itemId = item.ItemIds[0];
+                double itemPrice = item.Prices[0];
+                Console.WriteLine(item.MarketHashName + " " +  itemPrice);
+                List<DelistedItem> delistedItems = DelistFromSale.DelistItem(app, new List<string> { itemId });
+                Assert.AreEqual(itemId, delistedItems[0].ItemId);
+                List<RelistedItem> relistedItems = RelistForSale.RelistItem(app, new List<string> { itemId }, new List<double> { itemPrice });
+            }
+        }
+
+        [TestMethod]
+        public void ModifySaleTest()
+        {
+            AppId.AppName app = AppId.AppName.CounterStrikGlobalOffensive;
+            BitSkinsInventoryItem item = null;
+            foreach (AppId.AppName appId in Enum.GetValues(typeof(AppId.AppName)))
+            {
+                AccountInventory accountInventorys = Inventories.GetAccountInventory(appId, 1);
+                if (accountInventorys.BitSkinsInventory.TotalItems > 0)
+                {
+                    item = accountInventorys.BitSkinsInventory.BitSkinsInventoryItems[0];
+                    app = appId;
+                    break;
+                }
+            }
+
+            if (item != null)
+            {
+                string itemId = item.ItemIds[0];
+                double itemPrice = item.Prices[0];
+                double itemNewPrice = itemPrice * 5;
+                List<ModifiedItem> modifiedItems = ModifySaleItems.ModifySale(app, new List<string> { itemId }, new List<double> { itemNewPrice });
+                Assert.AreEqual(itemId, modifiedItems[0].ItemId);
+                modifiedItems = ModifySaleItems.ModifySale(app, new List<string> { itemId }, new List<double> { itemPrice });
+                Assert.AreEqual(itemId, modifiedItems[0].ItemId);
+            }
+        }
+
+        [TestMethod]
+        public void BuyItemTest()
+        {
+            AppId.AppName app = AppId.AppName.CounterStrikGlobalOffensive;
+            List<ItemOnSale> itemsOnSale = InventoryOnSale.GetInventoryOnSale(app, 1, "", 0, 0.01, InventoryOnSale.SortBy.Price,
+                        InventoryOnSale.SortOrder.Asc, InventoryOnSale.ThreeChoices.NotImportant, InventoryOnSale.ThreeChoices.NotImportant,
+                        InventoryOnSale.ThreeChoices.NotImportant, InventoryOnSale.ResultsPerPage.R30, InventoryOnSale.ThreeChoices.False);
+
+            if (itemsOnSale.Count > 0)
+            {
+                ItemOnSale item = itemsOnSale[0];
+                List<BoughtItem> boughtItems = Purchase.BuyItem(app, new List<string> { item.ItemId }, new List<double> { item.Price }, false, false);
+                string itemId = boughtItems[0].ItemId;
+                Assert.AreEqual(item.ItemId, itemId);
+            }
+        }
+
+        [TestMethod]
+        public void SellItemTest()
+        {
+            AppId.AppName app = AppId.AppName.CounterStrikGlobalOffensive;
+            SteamInventoryItem item = null;
+            foreach (AppId.AppName appId in Enum.GetValues(typeof(AppId.AppName)))
+            {
+                AccountInventory accountInventorys = Inventories.GetAccountInventory(appId, 1);
+                if (accountInventorys.SteamInventory.TotalItems > 0)
+                {
+                    item = accountInventorys.SteamInventory.SteamInventoryItems[0];
+                    app = appId;
+                    break;
+                }
+            }
+
+            if (item != null)
+            {
+                string itemId = item.ItemIds[0];
+                double itemPrice = item.SuggestedPrice * 5;
+                InformationAboutSale information = Sale.SellItem(app, new List<string> { itemId }, new List<double> { itemPrice });
+                string id = information.SoldItems[0].ItemId;
+                Assert.AreEqual(itemId, id);
             }
         }
     }
