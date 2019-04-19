e8abdd0ae36cc098411ac650057f15e671206449
Dmitry <danilindmitr99@icloud.com>
2019-04-19 08:58:14 +0300

    Update license

--- src/BitSkinsApi/Account/AccountData.cs
+++ src/BitSkinsApi/Account/AccountData.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * BitSkinsApi
- * Copyright (C) 2019 Dmitry
+ * Copyright (C) 2019 Captious99
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
