4255759687469d5f2704fbdf279a6ce3400e518d
Dmitry <danilindmitr99@icloud.com>
2019-04-20 19:51:03 +0300

    Add Crypto tests

---
+++ src/BitSkinsApiTests/Tests/ServerRequest/CryptoTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitSkinsApi.Crypto;
+
+namespace BitSkinsApiTests.ServerRequest
+{
+    [TestClass]
+    public class CryptoTests
+    {
+        [TestMethod]
+        public void GetBitcoinDepositAddressTest()
+        {
+            AccountBitcoinDepositAddress.GetBitcoinDepositAddress();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void GetBitcoinDepositRateTest()
+        {
+            BitcoinConversionRate.GetBitcoinDepositRate();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void CreateBitcoinDepositTest()
+        {
+            BitcoinDepositRate bitcoinDepositRate = BitcoinConversionRate.GetBitcoinDepositRate();
+            double usd = Math.Round(bitcoinDepositRate.PricePerBitcoinInUsd * 0.001, 2);
+            CreatingBitcoinDeposit.CreateBitcoinDeposit(usd);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
