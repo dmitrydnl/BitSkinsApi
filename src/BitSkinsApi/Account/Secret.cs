7e8cc0a5369b7497da35d31301c3e455fb583ca7
Dmitry <danilindmitr99@icloud.com>
2019-06-29 01:54:40 +0300

    Update license headers

--- src/BitSkinsApi/Account/Secret.cs
+++ src/BitSkinsApi/Account/Secret.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * BitSkinsApi
- * Copyright (C) 2019 Captious99
+ * Copyright (C) 2019 dmitrydnl
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/BitSkinsApi/BuyOrder/CancelBuyOrders.cs
+++ src/BitSkinsApi/BuyOrder/CancelBuyOrders.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * BitSkinsApi
- * Copyright (C) 2019 Captious99
+ * Copyright (C) 2019 dmitrydnl
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using BitSkinsApi.Market;
